6d6e01523177fa4858745a3402172310c7567684
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-03 13:57:29 -0500

    added entangle function

--- Algorithms/Teleportation.py
+++ Algorithms/Teleportation.py
@@ -1,3 +1,14 @@
 import cirq
-import math
+from cirq.circuits import InsertStrategy
 
+qbits = cirq.LineQubit.range(3)
+quantum_circuit = cirq.Circuit()
+
+
+def entangle(a, b, circuit):
+    circuit.append(cirq.H(a), strategy=InsertStrategy.NEW_THEN_INLINE)
+    circuit.append(cirq.CNOT(a, b))
+
+
+entangle(qbits[0], qbits[2], quantum_circuit)
+print(quantum_circuit)
