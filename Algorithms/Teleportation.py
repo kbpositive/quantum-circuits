dc4a4ac5f31f0fe5c927d4118ea9160d2e757fdb
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-17 15:47:25 -0500

    refactoring

--- Algorithms/Teleportation.py
+++ Algorithms/Teleportation.py
@@ -6,10 +6,28 @@ qbits = cirq.LineQubit.range(3)
 quantum_circuit = cirq.Circuit()
 
 
-def entangle(a, c, circuit):
-    circuit.append(cirq.H(a), strategy=InsertStrategy.NEW_THEN_INLINE)
-    for b in c:
-        circuit.append(cirq.CNOT(a, b))
+def entangle(initialBit, targetBits, circuit):
+    circuit.append(cirq.H(initialBit), strategy=InsertStrategy.NEW_THEN_INLINE)
+    for bit in targetBits:
+        circuit.append(cirq.CNOT(initialBit, bit))
+
+
+def encodeData(entangledBit, newBit, circuit):
+    # connect new qubit with entangled bit
+    circuit.append(cirq.CNOT(newBit, entangledBit))
+    # add H gave to new qubit for phase correction
+    circuit.append(cirq.H(newBit))
+    # measure entangled bit and new bit
+    circuit.append(cirq.measure(newBit))
+    circuit.append(cirq.measure(entangledBit))
+    # teleported data
+    return entangledBit, newBit
+
+
+def update(xFlip, zFlip, entangledBit, circuit):
+    # information from new bit
+    circuit.append(cirq.CNOT(xFlip, entangledBit))
+    circuit.append(cirq.CZ(zFlip, entangledBit))
 
 
 # teleported bit
@@ -18,19 +36,11 @@ quantum_circuit.append(cirq.X(qbits[0]))
 # start with entangled pair
 entangle(qbits[1], qbits[2:], quantum_circuit)
 
-# connect new qubit with entangled bit
-quantum_circuit.append(cirq.CNOT(qbits[0], qbits[1]))
+# encode new bit with entangled bit
+xf, zf = encodeData(qbits[1], qbits[0], quantum_circuit)
 
-# add H gave to new qubit for phase correction
-quantum_circuit.append(cirq.H(qbits[0]))
-
-# measure entangled bit and new bit
-quantum_circuit.append(cirq.measure(qbits[0]))
-quantum_circuit.append(cirq.measure(qbits[1]))
-
-# information from new bit
-quantum_circuit.append(cirq.CNOT(qbits[1], qbits[2]))
-quantum_circuit.append(cirq.CZ(qbits[0], qbits[2]))
+# update other entangled bit with new (teleported) info
+update(xf, zf, qbits[2], quantum_circuit)
 
 # measure results
 quantum_circuit.append(cirq.measure(*qbits, key="result"))
